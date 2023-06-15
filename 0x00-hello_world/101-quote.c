b4eda5d40f17874e9de27ba27ee4daee1e57f74c
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-15 12:43:04 -0700

    Add Advanced Tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print to the standard error
+ * Return: 1 if ran properly
+ */
+int main(void)
+{
+	write(2, and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1); 
+}
