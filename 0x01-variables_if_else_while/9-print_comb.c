0b25f78f8db6bbc606418d22a6849d2068f5c982
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-18 18:40:24 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combs of single-digit numbers
+ * Return: 0 when ran properly
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
