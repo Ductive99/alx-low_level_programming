e0a8c9415f934068c7e675eecb0f3164b28e537b
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-18 17:10:52 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase using putchar
+ * Return: 0 when ran properly
+ */
+int main(void)
+{
+	putchar('abcdefghijklmnopqrstuvwxyz');
+	return (0);
+}
