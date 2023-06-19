759b54883042b41b123691803f93439a4f119cd5
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-18 18:59:23 -0700

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - pritn all possible combs of two digits
+ * Return: 0 when ran properly
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+	
+
+	for (n1 = 48; n1 < 58; n1++)
+	{
+		for (n2 = n1 + 1; n2 < 58; n2++)
+		{
+			putchar(n1);
+			putchar(n2);
+			if (n1 != 56)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
