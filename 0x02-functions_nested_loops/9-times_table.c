4a10ac6747aefc308aeccf402d88b2efb758e70b
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-20 16:13:56 -0700

    Task 9 quick fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@ void times_table(void)
 	int n1;
 	int n2;
 
-	for (n1 = 0; n < 10; n++)
+	for (n1 = 0; n1 < 10; n1++)
 	{
-		for (n2 =  0; n < 10; n++)
+		for (n2 =  0; n2 < 10; n2++)
 		{
 			if (n1 * n2 < 10)
 				_putchar('0' + n1 * n2);
