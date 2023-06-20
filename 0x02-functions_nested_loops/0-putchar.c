cb1a44b2fbbb4fbbb228a2cf992fb9822e1e77f3
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-20 10:52:13 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print "_putchar" using putchar
+ * Return: 0 when ran properly
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
