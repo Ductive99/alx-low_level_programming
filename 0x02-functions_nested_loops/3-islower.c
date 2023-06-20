5ebfb9d9602caaa5ea969276f074de553ec653d4
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-20 14:13:18 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks whether a character is lowercase or not
+ * Return: 1 if chharacter is lowercase
+ * Return: 0 if not
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
