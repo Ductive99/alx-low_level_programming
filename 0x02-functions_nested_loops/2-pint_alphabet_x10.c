c51503f18d702ada98d14a12306ef9cd968c4611
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-20 11:31:35 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-pint_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the aplphabet 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+	_putchar('\n');
+}
