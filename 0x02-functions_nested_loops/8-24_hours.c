8cccd214d0c77064e37af301ff2e6456a788a1ae
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-20 15:58:28 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "min.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer, from 00:00 to 23:59
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar('0' + h / 10);
+			_putchar('0' + h % 10);
+			_putchar(':');
+			_putchar('0' + m / 10);
+			_putchar('0' + m % 10);
+			_putchar('\n');
+		}
+	}
+}
