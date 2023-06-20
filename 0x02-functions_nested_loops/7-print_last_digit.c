c29d651fe331d993c47426b7cc183e41303878f3
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-20 15:49:20 -0700

    Task 7 Fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,11 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-		n = -n;
-
 	last_digit = n % 10;
 
+	if (last_digit < 0)
+		last_digit = -last_digit;
+
 	_putchar('0' + last_digit);
-	return (n % 10);
+	return (last_digit);
 }
