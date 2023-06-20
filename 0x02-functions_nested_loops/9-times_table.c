c02615fbdca4e8ab3900046090c3730ddf7aa0fa
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-20 16:19:49 -0700

    Task 9 fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,11 @@ void times_table(void)
 		for (n2 =  0; n2 < 10; n2++)
 		{
 			if (n1 * n2 < 10)
+			{
 				_putchar('0' + n1 * n2);
+				if (n2 > 0)
+					_putchar(' ');
+			}
 			else
 			{
 				_putchar('0' + (n1 * n2) / 10);
