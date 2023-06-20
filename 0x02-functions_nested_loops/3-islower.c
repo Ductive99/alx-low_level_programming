4ef11ea86e0b60dfcb2aa7fceaa1af28e9746a57
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-20 14:17:12 -0700

    Comment modification

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,7 @@
 
 /**
  * _islower - checks whether a character is lowercase or not
- * Return: 1 if chharacter is lowercase
- * Return: 0 if not
+ * Return: 1 if chharacter is lowercase, 0 if not
  */
 int _islower(int c)
 {
