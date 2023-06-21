5d5fbe94e2e0e7beaa3ab99fa2de9fc1d51ce630
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-20 19:00:52 -0700

    Task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints thesum of all even terms of Fibonacci numbers under 4 million
+ * Return: 0
+ */
+int main(void)
+{
+	long temp;
+	long n0 = 1;
+	long n1 = 2;
+	long sum = 2;
+
+	while (n0 + n1 < 40000000)
+	{
+		temp = n0 + n1;
+		printf("%ld", temp);
+		n0 = n1;
+		n1 = temp;
+		if (n1 % 2 == 0)
+			sum += n1;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
