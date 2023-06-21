5f2dcf6f33c5f059eb707b772bd0ffe44c4fb6e5
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-20 18:01:45 -0700

    Fingers crossed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - prints the n times table starting with 0
- * @n; 0 < n <= 15
+ * @n;integer between 0 and 15
  * Return: 0
  */
 void print_times_table(int n)
@@ -37,7 +37,7 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar('0' + (n1 * n2) / 100);
-					_putchar('0' + (n1 * n2) % 100);
+					_putchar('0' + ((n1 * n2) % 100) / 10);
 					_putchar('0' + (n1 * n2) % 10);
 				}
 				if (n2 < n)
