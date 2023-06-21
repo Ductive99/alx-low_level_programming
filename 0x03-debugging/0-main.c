11b441823840a761626e942df61abced1ed2209e
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-21 04:51:30 -0700

    Initial commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests the fumction positive_or_negative
+ * Return: 0 when ran properly
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
