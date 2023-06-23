0fe7e9baeee0356ca20c355f63da491949c58ad9
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-23 11:35:11 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,9 +15,13 @@ void print_triangle(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size - i - 1; j++)
+			{
 				_putchar(' ');
+			}
 			for (z = 0; z < size - j; z++)
+			{
 				_putchar('#');
+			}
 
 			_putchar('\n');
 		}
