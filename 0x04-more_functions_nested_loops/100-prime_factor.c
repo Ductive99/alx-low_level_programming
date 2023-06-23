b18619f3633305270a84da1699efa116e82492a4
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-23 12:14:02 -0700

    Advanced Task 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * main - find and print the largest prim efactor of 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+	long i, largest_prime_factor;
+	long n;
+
+	n = 612852475143;
+	largest_prime_factor = 1;
+	
+	for (i = 1; i < sqrt(n); i++)
+	{
+		if (i % n == 0)
+			largest_prime_factor = i;
+	}
+	printf("%ld\n", largest_prime_factor);
+}
