b32bac37e21e3fe43f5654642e2765a9c1e5b3a0
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-26 15:43:11 -0700

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates a value of an int through a pointer
+ * @n: pointer to the variable to be updated
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
