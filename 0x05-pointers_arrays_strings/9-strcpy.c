9017d2a8fecc2ef124be97b0414e8274876d0889
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-27 08:41:29 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,14 +11,16 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	char *buffer = dest;
 
 	i = 0;
 
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	dest[i] = '\0';
 
-	return (dest);
+	return (buffer);
 }
