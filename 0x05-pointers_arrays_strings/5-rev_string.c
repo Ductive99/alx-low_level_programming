38f9261e48a740b37a1beb1eccf7fee40d06e25c
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-27 06:39:20 -0700

    Code fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,42 +5,26 @@
  * @s: string to be reversed
  * Return: void
  */
-int size(char *s);
 
 void rev_string(char *s)
 {
 	int i, j;
 	int len;
-	char temp[size(s)];
+	char temp;
 
 	i = 0;
 	j = 0;
 
-	len = size(s);
+	while (s[len] != '\0')
+		len++;
+
+	j = len - 1;
 
 
-	for (i = i - 1; i >= 0; i--)
+	for (i = 0; i < len / 2; i++)
 	{
-		*(temp + j) = *(s + i);
-		j++;
+		temp = s[i];
+		s[i] = s[j]
+		s[j--] = temp;
 	}
-	for (j = 0; j < len; j++)
-		*(s + j) = *(temp + j);
-}
-
-/**
- * size - returns the length of a string
- * @s: that string
- * Return: the strings length
- */
-
-int size(char *s)
-{
-	int i;
-
-	i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
 }
