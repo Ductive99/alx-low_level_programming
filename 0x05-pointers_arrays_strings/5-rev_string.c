150f1f1aa131ea4197e83d7c8c89c0afe489abf4
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-27 06:08:35 -0700

    Task 5: String reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+	int len;
+	char *temp;
+
+	i = 0;
+	j = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+
+	len = i;
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		*(temp + j) = *(s + i);
+		j++;
+	}
+	for (j = 0; j < len; j++)
+		*(s + j) = *(temp + j);
+}
