ef6a0c64fc1b686cae9a6f4c8ca9d276277c3697
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-30 15:46:42 -0700

    Task 4: reverse styling fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,7 +12,7 @@ void reverse_array(int *a, int n)
 	int i;
 	int temp;
 
-	for (i = 0; i < n/2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
 		a[i] = a[n - i - 1];
@@ -20,8 +20,8 @@ void reverse_array(int *a, int n)
 	}
 	if (n % 2 == 1)
 	{
-                temp = a[i];
-                a[i] = a[n - i - 1];
-                a[n - i - 1] = temp;
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
 	}
 }
