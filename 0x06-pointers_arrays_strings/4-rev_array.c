23739658b0a531cb721ea276f0d6ad8f47e12f30
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-30 15:42:53 -0700

    Task 4: reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content of an array of integers
+ * @a: the array of integers
+ * @n: the number of elements
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0; i < n/2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+	if (n % 2 == 1)
+	{
+                temp = a[i];
+                a[i] = a[n - i - 1];
+                a[n - i - 1] = temp;
+	}
+}
