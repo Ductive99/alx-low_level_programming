769da135489f5e62898beb9570fdc19c0218ad30
El Houssain Souhail <sou.ductive@gmail.com>
2023-06-30 11:30:58 -0700

    Task 0: strcat fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,20 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	char *pdest = dest;
 
 	i = 0;
+	j = 0;
 
 	while (dest[i] != '\0')
 		i++;
 
-	for (; src[j] != '\0'; i++)
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
+		i++;
 	}
 	dest[i] = '\0';
 
-	return (pdest);
+	return (dest);
 }
