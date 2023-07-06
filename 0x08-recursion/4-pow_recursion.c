99e7d2d14f068051e357b1dc92b8ca086db76f25
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-06 08:47:06 -0700

    Task 4 - Power function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns x to the power of y
+ * @x: the base
+ * @y: the power
+ * Return: the result of x^y
+ * y < 0 => return (-1)
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+
+	return (x * _pow_recursion(y - 1));
+}
