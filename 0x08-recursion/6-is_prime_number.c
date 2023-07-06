2f5bc381f80c67ed14bdb04c0ca9de01c625ef4e
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-06 11:24:55 -0700

    Task 6 - Prime checker

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int checker(int n, int i);
+
+/**
+ * is_prime_number - checks whether a number is prime or not
+ * @n: the number to check
+ * Return: 1 -> prime, 0 -> otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n % 2 || n < 2)
+		return (0);
+
+	return (checker(n, 3));
+}
+
+/**
+ * checker - checks starting with i whether n is divisible by i
+ * @n: the number to check
+ * @i: the divisor
+ * Return: 0 if n is divisibal by i, 1 otherwise
+ */
+
+int checker(int n, int i)
+{
+	if (n % i == 0)
+		return (0);
+	if (i > n / 2)
+		return (0);
+
+	return (checker(n, i + 2));
+}
