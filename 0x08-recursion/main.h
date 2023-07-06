699f920bb1e9168e8770d10a4d16d037c7bb006a
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-06 07:34:11 -0700

    Task 0 - Recursive string print

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_recursion(char *s);
+int _srlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqr_recursion(int n);
+int is_prime_number(int n);
+
+#endif /* HEADER_H */
