d3e2f21048158abd37563644d3f5d150c567846f
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-06 16:26:54 -0700

    Advanced Task 101 - String compare

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares 2 strings
+ * @s1: the 1st string
+ * @s2: the 2nd string
+ * Return: 1 if identical, 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 != *s2 && *s2 != '*')
+		return (0);
+
+	return (wildcpm(s1 + 1, s2 + 1));
+}
