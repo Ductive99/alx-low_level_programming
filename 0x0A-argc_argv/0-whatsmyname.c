0fa279857ab3df18b79d7b6f7504749154c1d1c4
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-07 14:21:54 -0700

    Task 0 - Program name print

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program's name
+ * Return: 0 when ran successfuly
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
