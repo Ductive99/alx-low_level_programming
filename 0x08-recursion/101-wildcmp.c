95777dfd5504b0ee5fc42f162a96641ee7fe3bd6
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-06 19:20:51 -0700

    Advanced Task 101 - Debugging

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,8 +11,15 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	else if (*s1 != *s2 && *s2 != '*')
+
+	if (*s2 != '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		return (0);
 
-	return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
+	return (0);
 }
