5a62113c18d8384a6e4e1b2e9271f0ce94375c37
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-11 10:13:54 -0700

    Task 1 - Different way to copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of a given string
+ * @str: the string to be copied
+ * Return: NULL if str = NULL
+ * On success, returns a pointer to the duplicated string.
+ * It returns NULL if insufficient memory was available
+ */
+
+char *_strdup(char *str)
+{
+	int i, len;
+	char *new;
+
+	len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	len = i;
+
+	new = malloc(sizeof(char) * len);
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		new[i] = str[i];
+
+	return (new);
+}
