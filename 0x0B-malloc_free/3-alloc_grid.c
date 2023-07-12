65196b69d232721d15d8214309bfb1a790caee02
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-11 19:19:03 -0700

    Task 3 - fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
