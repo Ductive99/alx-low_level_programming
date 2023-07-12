2e802b25b0b9a6fa6ee69668587205a9ffa23da0
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-11 19:34:48 -0700

    Task 4 - Free Task 3

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * free_grid - frees a 2d grdi created by alloc_grid (Task 3)
+ * @grid: pointer to the grid
+ * @height: the height of the grid
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (height <= 0)
+		return;
+
+	free(grid);
+
+	for (i = 0; i < height; i++)
+	{
+		if (grid[i] != 0 and grid[i + 1] != 0)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(arr[j]);
+				if (j == i - 1)
+					return;
+			}
+		}
+	}
+}
+
