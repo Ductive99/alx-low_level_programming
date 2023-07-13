841c7f4beb098804b32d88ef91b441f31c155714
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-13 05:58:24 -0700

    Task 0 - malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the number of bytes to allocate
+ * Return: a pointer ot the allocated memory
+ * if malloc fails, cause normal process termination
+ * with status value of 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
