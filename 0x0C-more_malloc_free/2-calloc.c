b9578c6e4b62805d3a4295ed8f787e1aed5b70ce
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-13 08:13:56 -0700

    Task 2 - Array Memory Allocation

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -24,7 +24,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	for (i = 0; i < nmemb * size; i++)
-		(char *)ptr[i] = 0;
+		*((char *)(ptr) + i) = 0;
 
 	return (ptr);
 }
