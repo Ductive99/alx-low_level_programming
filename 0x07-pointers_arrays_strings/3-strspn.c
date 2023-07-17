cc733942fe9fca2b8611fc55d9df45db063aba13
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-17 08:20:42 -0700

    Updated README

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - gets the negth of a prefix substring
+ * @s: the string
+ * @accept: the prefix
+ * Return: the number of bytes in the initial segment of s
+ * which consists only of bytes from accept
+ */
