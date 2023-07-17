2edd44e5d8bf87b7aaaf0ea7b72bbdbde89bf7c6
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-17 09:04:07 -0700

    Added Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog variables
+ * @name: name of the dog
+ * @age:   age of the dog
+ * @owner: name of the owner of the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 #endif /* #ifndef DOG_H */
