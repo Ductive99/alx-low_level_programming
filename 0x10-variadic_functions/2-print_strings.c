0d8411b17b1f8780d25acaa9e30ec9455e5a6870
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-21 11:39:01 -0700

    Added task 3 style fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (s == NULL)
 			printf("(nil)");
-		else 
+		else
 			printf("%s", s);
 
 		if (separator != NULL && i != n - 1)
