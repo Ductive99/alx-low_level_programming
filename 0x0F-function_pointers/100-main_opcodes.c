919a4280d33d2dd3f6ed5f932876fd54d8c38b66
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-21 10:11:38 -0700

    Added advanced task 100 - opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_opc(char *s, int n);
+
+/** main - prints the opcodes of its own main function
+ *
+ * @argc: number of arguments
+ * @argv: the arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	print_opc((char *)&main, n);
+
+	return (0)
+}
+
+/**
+ * print_opc - prints opcodes of main
+ * @s: address of main
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
+
+void print_opc(char *s, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+}
