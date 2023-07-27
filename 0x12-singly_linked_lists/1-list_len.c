6fe2bf02d9db12a723851881a709c95c06d25c50
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-27 10:12:59 -0700

    added task 1 - list element counter

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list - counts the number of elements of a list_t list
+ * list_t list is a singly linked list
+ *
+ * @h: pointer to the list
+ *
+ * Return: the number of elements/nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node_count;
+
+	for (node_cout = 0; h != NULL; node_count++)
+		h = h->next;
+
+	return (node_count);
+}
