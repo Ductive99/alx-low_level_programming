cd5dd79e9bb9cd09fc7e360049f0e9c648c1aab3
El Houssain Souhail <sou.ductive@gmail.com>
2023-07-27 09:45:10 -0700

    added task 0 - list print

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+void print_count(size_t n);
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * list_t list is a singly linked list
+ *
+ * @h: pointer to the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		node_count++;
+		h = h->next;
+	}
+
+	return (node_count);
+}
