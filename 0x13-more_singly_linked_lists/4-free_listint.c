8ca70b70ef7c32b645cb19ae4bdb7671306e0c97
El Houssain Souhail <sou.ductive@gmail.com>
2023-08-01 09:05:25 -0700

    task 4 - steps fix

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,12 +9,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *temp = head->next;
+	listint_t *temp;
 
 	while (head)
 	{
+		temp = head->next;
 		free(head);
 		head = temp;
-		temp = head->next;
 	}
 }
