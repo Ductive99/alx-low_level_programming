d304fc3fffe5297ea22231f736cec33e696b5e9c
El Houssain Souhail <sou.ductive@gmail.com>
2023-08-01 10:48:14 -0700

    task 9 - handling edge cases

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,11 +22,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
 	for (i = 0; temp && i < idx - 1; i++)
 		temp = temp->next;
 
 	if (i != idx - 1)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	new->n = n;
 	new->next = temp->next;
