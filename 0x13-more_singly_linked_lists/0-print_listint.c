bef10839fc5582366e115474b0f58305c5231c26
El Houssain Souhail <sou.ductive@gmail.com>
2023-08-01 05:11:11 -0700

    added task 0 - print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elemts of a list
+ * @h: a listint list (singly-linked list)
+ *
+ * Return: number of nodes;
+ */
+
+size_t print_listint(const listint_t *h);
+{
+	size_t count = 0;
+	
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	
+	return (count);
+}
