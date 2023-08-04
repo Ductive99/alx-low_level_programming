ed99cafd930e3eb983a23ced9e4904d8e746429d
El Houssain Souhail <sou.ductive@gmail.com>
2023-08-04 09:57:47 -0700

    task 1 - unsigned long int to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,16 +10,16 @@
 
 void print_binary(unsigned long int n)
 {
-	int bitCount = sizeof(unsigned int) * 8;
-	int leadingZero = 1;
+	int bitCount = sizeof(unsigned long int) * 8;
+	int leadingZero = 1, i;
     
-	for (int i = bitCount - 1; i >= 0; i--)
+	for (i = bitCount - 1; i >= 0; i--)
 	{
-		unsigned int mask = 1u << i;
-		if ((num & mask) || !leadingZero) 
+		unsigned long int mask = 1ul << i;
+		if ((n & mask) || !leadingZero) 
 		{
 			leadingZero = 0;
-			_putchar((num & mask) ? '1' : '0');
+			_putchar((n & mask) ? '1' : '0');
 		}
 	}
 
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _strlen(const char *s);
+int _putchar(char c);
 
 #endif /* #ifnded HEADER_H */
