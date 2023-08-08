bc4b9183666a95d7137487476671bd8c1aebd8a8
ductive <sou.ductive@gmail.com>
2023-08-08 01:06:24 +0100

    added task 1 - file creator

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,24 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, i;
-    int j = 0;
+	int fd, i;
+	int j = 0;
 
-    if (!filename)
-        return (-1);
-    
-    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    if (fd < 0)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    if (text_content)
-    {
-        while (text_content[j])
-            j++;
-        i = write(fd, text_content, j);
-        if (i != j)
-            return (-1);
-    }
-    close(fd);
-    return (1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[j])
+			j++;
+		i = write(fd, text_content, j);
+		if (i != j)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }
