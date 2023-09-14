67c2b8cfe0bcb3c383bb866ba0b5dd9238500ca6
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-14 17:26:16 +0100

    debugging

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,9 +23,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (idx == 0 || *h == NULL)
-		return (add_dnodeint(h, n));
+	{
+		new = add_dnodeint(h, n);
+		if (!new)
+			return (NULL);
+		return (new);
+	}
 	else if (p->next == NULL)
-		return (add_dnodeint_end(h, n));
+	{
+		new = add_dnodeint_end(h, n);
+		if (!new)
+			return (NULL);
+		return (new);
+	}	
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
