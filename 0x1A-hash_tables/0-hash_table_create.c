5b6222aa3654ad85290f214d59446af1571e4238
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-06 19:30:42 +0100

    added task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 	table->array = malloc(size * sizeof(hash_node_t *));
-	if (!(table->array))
+	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
