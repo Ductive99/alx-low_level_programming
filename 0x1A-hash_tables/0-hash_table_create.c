d3e9baa3c691b484ce9105a156868c09a911c018
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-07 00:22:49 +0100

    betty styling

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
 	table->array = malloc(size * sizeof(hash_node_t *));
+
 	if (table->array == NULL)
 		return (NULL);
 
