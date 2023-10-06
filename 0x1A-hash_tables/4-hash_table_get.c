ea4d1c90993d5355420fcfe35d475da8f16e7565
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-06 20:20:55 +0100

    added task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value given a key
+ * @ht: the hash table to look into
+ * @key: the key to find
+ *
+ * Return: tha value associated with the key
+ * or NULL if the key couldn't be found
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (!ht)
+		return (NULL);
+
+	index = key_index((const unsigned char *) key, ht->size);
+	node = ht->array[index];
+
+	while(node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+
+		node = node->next;
+	}
+
+	return (NULL);
+}
