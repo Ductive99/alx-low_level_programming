097ff9644d0f02327c3facb789ab85b1dc2d4940
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-06 20:22:51 +0100

    task 4 - styling fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -19,7 +19,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *) key, ht->size);
 	node = ht->array[index];
 
-	while(node)
+	while (node)
 	{
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
