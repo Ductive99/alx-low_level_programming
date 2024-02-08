aed3b3b79b018c74f9cdd7c3990dbdf67a5583b0
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-08 15:41:47 +0100

    Added standard libraries

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,9 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
+#include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
