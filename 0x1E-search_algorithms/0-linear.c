9c7931b3665f57203a9e357dece30cae646d2b55
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-08 15:40:23 +0100

    Added action message

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,8 +18,11 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 
 	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
+	}
 
 	return (-1);
 }
