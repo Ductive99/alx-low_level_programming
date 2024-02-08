c6a69ab94d08991f89f93a51dd2469d6b4095361
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-08 15:28:01 +0100

    Added linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+
+/**
+ * linear_search - implements a linear search
+ *
+ * @array: pointer to the first element of the array to search in.
+ * @size: number of elements in @array.
+ * @value: value to search for
+ *
+ * Return: 1st index where @value is located, else return (-1).
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < (int)size; i++)
+		if (array[i] == value)
+			return (i);
+
+	return (-1);
+}
